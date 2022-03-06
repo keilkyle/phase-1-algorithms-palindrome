58850464704ad7693c4e92c492bc6ee296fe3191
Christian Keil <christian@astranis.com>
2022-03-05 22:25:40 -0800

    commit tree 4bdfe9c39ed5681af97ce9946feefa96fb4b340c

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function isPalindrome(word) {
+  let len = word.length
+  let result = true 
+  for (let pair = 0; pair < Math.floor(len/2); pair++) {
+    if(word[pair] === word[(len-pair-1)]) {} else {return !!!result}
+  }
+  return !!result
   // Write your algorithm here
 }
 
-/* 
-  Add your pseudocode here
-*/
-
 /*
-  Add written explanation of your solution here
+  Compare first and last letter in string
+  Then compare second and second to last letter in string
+  Repeat (length of string / 2) times, rounded down
+  If always a match, true, else false  
 */
 
 // You can run `node index.js` to view these console logs
